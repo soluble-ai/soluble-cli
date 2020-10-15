e432dad683695a2ed9e88ae910d42a09e03f5bc4
hemanth gokavarapu <hemanth@soluble.ai>
2020-10-15 12:18:53 +0530

    use snake case for the output
    Signed-off-by: hemanth gokavarapu <hemanth@soluble.ai>

--- pkg/results/types.go
+++ pkg/results/types.go
@@ -2,15 +2,15 @@ package results
 
 // Violation Contains data for each violation
 type Violation struct {
-	RuleName     string      `json:"rule_name" yaml:"rule_name" xml:"rule_name,attr"`
+	RuleName     string      `json:"ruleName" yaml:"rule_name" xml:"rule_name,attr"`
 	Description  string      `json:"description" yaml:"description" xml:"description,attr"`
-	RuleID       string      `json:"rule_id" yaml:"rule_id" xml:"rule_id,attr"`
+	RuleID       string      `json:"ruleId" yaml:"rule_id" xml:"rule_id,attr"`
 	Severity     string      `json:"severity" yaml:"severity" xml:"severity,attr"`
 	Category     string      `json:"category" yaml:"category" xml:"category,attr"`
 	RuleFile     string      `json:"-" yaml:"-" xml:"-"`
 	RuleData     interface{} `json:"-" yaml:"-" xml:"-"`
-	ResourceName string      `json:"resource_name" yaml:"resource_name" xml:"resource_name,attr"`
-	ResourceType string      `json:"resource_type" yaml:"resource_type" xml:"resource_type,attr"`
+	ResourceName string      `json:"resourceName" yaml:"resource_name" xml:"resource_name,attr"`
+	ResourceType string      `json:"resourceType" yaml:"resource_type" xml:"resource_type,attr"`
 	ResourceData interface{} `json:"-" yaml:"-" xml:"-"`
 	File         string      `json:"file" yaml:"file" xml:"file,attr"`
 	LineNumber   int         `json:"line" yaml:"line" xml:"line,attr"`
