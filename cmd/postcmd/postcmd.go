bc3d35cdd67c799fef7113152f8c12660c61e309
Matt Hamilton <matt@soluble.ai>
2020-10-19 17:41:11 -0400

    add XCP Post w/ env convenience function (#76)
    Add -e option to post command to include CI environment
    
    Signed-off-by: Matt Hamilton <matt@soluble.ai>
    
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>
    
    Co-authored-by: Sam Shen <slshen@users.noreply.github.com>

--- cmd/postcmd/postcmd.go
+++ cmd/postcmd/postcmd.go
@@ -1,26 +1,33 @@
 package postcmd
 
 import (
+	"github.com/soluble-ai/soluble-cli/pkg/client"
 	"github.com/soluble-ai/soluble-cli/pkg/options"
 	"github.com/spf13/cobra"
 )
 
 func Command() *cobra.Command {
 	var (
-		module string
-		files  []string
-		values map[string]string
+		module  string
+		files   []string
+		values  map[string]string
+		withEnv bool
 	)
 	opts := options.ClientOpts{}
 	c := &cobra.Command{
 		Use:   "post",
 		Short: "Send data to soluble",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return opts.GetAPIClient().XCPPost(opts.GetOrganization(), module, files, values)
+			var options []client.Option
+			if withEnv {
+				options = []client.Option{client.WithCIEnv}
+			}
+			return opts.GetAPIClient().XCPPost(opts.GetOrganization(), module, files, values, options...)
 		},
 	}
 	opts.Register(c)
 	flags := c.Flags()
+	flags.BoolVarP(&withEnv, "env", "e", false, "Include CI environment variables and git information.")
 	flags.StringVarP(&module, "module", "m", "", "The module to post under, required.")
 	flags.StringSliceVarP(&files, "file", "f", nil, "Send a file, can be repeated")
 	flags.StringToStringVarP(&values, "param", "p", nil, "Include a key value pair, can be repeated.  The argument should be in the form key=value.")
---
+++ pkg/client/env.go
@@ -0,0 +1,119 @@
+package client
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const (
+	metaGitRemote string = "SOLUBLE_METADATA_GIT_REMOTE"
+	metaGitBranch string = "SOLUBLE_METADATA_GIT_BRANCH"
+	metaHostname  string = "SOLUBLE_METADATA_HOSTNAME"
+)
+
+var (
+	// We explicitly exclude a few keys due to their sensitive values.
+	// The substrings below will cause the environment variable to be
+	// skipped (not recorded).
+	substringOmitEnv = []string{
+		"SECRET", "KEY", "PRIVATE", "PASSWORD",
+		"PASSPHRASE", "CREDS", "TOKEN", "AUTH",
+		"ENC", "JWT",
+		"_USR", "_PSW", // Jenkins credentials()
+	}
+
+	// While we perform the redactions based on substrings above,
+	// we also maintain a list of known-sensitive keys to ensure
+	// that we never capture these. Unlike above, these are an
+	// exact match and not a substring match.
+	explicitOmitEnv = []string{
+		"BUILDKITE_S3_SECRET_ACCESS_KEY",
+		"BUILDKITE_S3_ACCESS_KEY_ID",
+		"BUILDKITE_S3_ACCESS_URL",
+		"KEY",                  // CircleCI encrypted-files decryption key
+		"CI_DEPLOY_PASSWORD",   // Gitlab
+		"CI_DEPLOY_USER",       // Gitlab
+		"CI_JOB_TOKEN",         // Gitlab
+		"CI_JOB_JWT",           // Gitlab
+		"CI_REGISTRY_USER",     // Gitlab
+		"CI_REGISTRY_PASSWORD", // Gitlab
+		"CI_REGISTRY_USER",     // Gitlab
+	}
+)
+
+var _ Option = WithCIEnv
+
+// Include CI-related environment variables in the request
+func WithCIEnv(req *resty.Request) {
+	req.SetMultipartFormData(getCIEnv())
+}
+
+func getCIEnv() map[string]string {
+	values := map[string]string{}
+	allEnvs := make(map[string]string)
+	for _, e := range os.Environ() {
+		split := strings.Split(e, "=")
+		allEnvs[split[0]] = split[1]
+	}
+	// We don't want all of the environment variables, however.
+envLoop:
+	for k, v := range allEnvs {
+		k = strings.ToUpper(k)
+		for _, s := range substringOmitEnv {
+			if strings.Contains(k, s) {
+				continue envLoop
+			}
+		}
+		for _, s := range explicitOmitEnv {
+			if k == s {
+				continue envLoop
+			}
+		}
+
+		// If the key has made it through the filtering above and is
+		// from a CI system, we include it.
+		if strings.HasPrefix(k, "GITHUB_") ||
+			strings.HasPrefix(k, "CIRCLE_") ||
+			strings.HasPrefix(k, "GITLAB_") ||
+			strings.HasPrefix(k, "CI_") ||
+			strings.HasPrefix(k, "BUILDKITE_") {
+			values[k] = v
+		}
+	}
+
+	// Git remote
+	cmd := exec.Command("git", "remote", "-v")
+	out, err := cmd.Output()
+	if err == nil {
+		entries := strings.Split(string(out), "\\n")
+		var remotes []string
+		for _, e := range entries {
+			startIdx := strings.Index(e, "\t")
+			endIdx := strings.Index(e, " ")
+			if startIdx == -1 || endIdx == -1 {
+				continue
+			}
+			remote := e[startIdx+1 : endIdx]
+			remotes = append(remotes, remote)
+		}
+		values[metaGitRemote] = remotes[0]
+	}
+
+	// Git Branch
+	cmd = exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	out, err = cmd.Output()
+	if err == nil {
+		values[metaGitBranch] = strings.TrimSpace(string(out))
+	}
+
+	// Hostname
+	h, err := os.Hostname()
+	if err == nil {
+		values[metaHostname] = h
+	}
+
+	return values
+}
