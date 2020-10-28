6c4d61635579bb74af3ceed334f21e0238614182
Matt Hamilton <matt@soluble.ai>
2020-10-28 15:11:06 -0400

    fix struct name
    Signed-off-by: Matt Hamilton <matt@soluble.ai>

--- pkg/tools/iacinventory/github_repo.go
+++ pkg/tools/iacinventory/github_repo.go
@@ -36,8 +36,8 @@ type GithubRepo struct {
 	// CloudformationDirs are directories that contain cloudformation files
 	CloudformationDirs []string `json:"cloudformation_dirs,omitempty"`
 
-	// DockerfileFiles are... Dockerfile files.
-	DockerfileFiles []string `json:"dockerfile_files,omitempty"`
+	// DockerfileDirs are... Dockerfile files.
+	DockerfileDirs []string `json:"dockerfile_files,omitempty"`
 
 	// K8sManifestDirs are directories that contain Kubernetes manifest files
 	K8sManifestDirs []string `json:"k8s_manifest_dirs,omitempty"`
@@ -140,7 +140,7 @@ func (g *GithubRepo) downloadAndScan(user, token string, repo *github.Repository
 
 	terraformDirs := util.NewStringSet()
 	cfnDirs := util.NewStringSet()
-	dockerFiles := util.NewStringSet()
+	dockerFileDirs := util.NewStringSet()
 	k8sManifestDirs := util.NewStringSet()
 	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -176,7 +176,7 @@ func (g *GithubRepo) downloadAndScan(user, token string, repo *github.Repository
 				cfnDirs.Add(pathRelativeToRepoRoot)
 			}
 			if isDockerFile(path, info) {
-				dockerFiles.Add(pathRelativeToRepoRoot)
+				dockerFileDirs.Add(pathRelativeToRepoRoot)
 			}
 			if isKubernetesManifest(path, info) {
 				k8sManifestDirs.Add(pathRelativeToRepoRoot)
@@ -189,9 +189,9 @@ func (g *GithubRepo) downloadAndScan(user, token string, repo *github.Repository
 	}
 	g.TerraformDirs = terraformDirs.Values()
 	g.CloudformationDirs = cfnDirs.Values()
-	g.DockerfileFiles = dockerFiles.Values()
+	g.DockerfileDirs = dockerFileDirs.Values()
 	g.K8sManifestDirs = k8sManifestDirs.Values()
 	log.Infof("{primary:%s} has {info:%d} terraform directories, {info:%d} cloudformation directories, {info:%d} dockerfiles, {info:%d} k8s manifest directories",
-		g.FullName, len(g.TerraformDirs), len(g.CloudformationDirs), len(g.DockerfileFiles), len(g.K8sManifestDirs))
+		g.FullName, len(g.TerraformDirs), len(g.CloudformationDirs), len(g.DockerfileDirs), len(g.K8sManifestDirs))
 	return nil
 }
