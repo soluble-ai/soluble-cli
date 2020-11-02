e378665237697fa675a2d9107464b594e4a4abb8
Sam Shen <slshen@users.noreply.github.com>
2020-11-02 11:48:44 -0800

    Use iac-scan subcommands instead of --scanner-type argument (#109)
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- cmd/iacscan/iacscan.go
+++ cmd/iacscan/iacscan.go
@@ -2,64 +2,28 @@ package iacscan
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/manifoldco/promptui"
 	"github.com/soluble-ai/soluble-cli/pkg/tools"
 	"github.com/soluble-ai/soluble-cli/pkg/tools/checkov"
 	"github.com/soluble-ai/soluble-cli/pkg/tools/terrascan"
 	"github.com/soluble-ai/soluble-cli/pkg/tools/tfsec"
+	"github.com/soluble-ai/soluble-cli/pkg/tools/trivy"
 	"github.com/spf13/cobra"
 )
 
-var supportedTools = []tools.Interface{
-	&terrascan.Tool{},
-	&tfsec.Tool{},
-	&checkov.Tool{},
-}
-
-func Command() *cobra.Command {
-	var (
-		directory   string
-		scannerType string
-	)
+func createCommand(tool tools.Interface) *cobra.Command {
 	opts := &tools.ToolOpts{}
-	toolNames := []string{}
-	for _, tool := range supportedTools {
-		toolNames = append(toolNames, tool.Name())
-	}
 	c := &cobra.Command{
-		Use:   "iac-scan",
-		Short: "Run an Infrastructure-as-code scanner",
+		Use:   tool.Name(),
+		Short: fmt.Sprintf("Scan infrastructure-as-code with %s", tool.Name()),
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if scannerType == "" {
-				prompt := promptui.Select{
-					Label: "Select Tool",
-					Items: toolNames,
-				}
-				_, selection, err := prompt.Run()
-				if err != nil {
-					return err
-				}
-				scannerType = selection
-			}
-			var tool tools.Interface
-			for _, t := range supportedTools {
-				if t.Name() == scannerType {
-					tool = t
-					break
-				}
-			}
-			if tool == nil {
-				return fmt.Errorf("unknown scanner type %s, must be one of: %s", scannerType,
-					strings.Join(toolNames, " "))
-			}
 			if u, ok := tool.(tools.RunsInDirectory); ok {
-				if directory == "" {
-					return fmt.Errorf("the %s scanner requires a directory to run in", scannerType)
+				d, _ := cmd.Flags().GetString("directory")
+				if d == "" {
+					return fmt.Errorf("%s requires --directory", tool.Name())
 				}
-				u.SetDirectory(directory)
+				u.SetDirectory(d)
 			}
 			if u, ok := tool.(tools.RunsWithAPIClient); ok {
 				u.SetAPIClient(opts.GetAPIClient())
@@ -69,8 +33,27 @@ func Command() *cobra.Command {
 	}
 	opts.Register(c)
 	flags := c.Flags()
-	flags.StringVarP(&directory, "directory", "d", "", "Directory to scan")
-	flags.StringVar(&scannerType, "scanner-type", "",
-		fmt.Sprintf("The scanner to use, should be one of: %s", strings.Join(toolNames, " ")))
+	if _, ok := tool.(tools.RunsInDirectory); ok {
+		flags.StringP("directory", "d", "", "Directory to scan")
+	}
+	if _, ok := tool.(tools.RunsWithAPIClient); !ok {
+		opts.AuthNotRequired = true
+	}
+	return c
+}
+
+func Command() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "iac-scan",
+		Short: "Run an Infrastructure-as-code scanner",
+		Example: `  # run the default scanner in the current directory
+  iac-scan default -d .`,
+	}
+	t := createCommand(&terrascan.Tool{})
+	t.Aliases = []string{"default"}
+	c.AddCommand(t)
+	c.AddCommand(createCommand(&checkov.Tool{}))
+	c.AddCommand(createCommand(&tfsec.Tool{}))
+	c.AddCommand(createCommand(&trivy.Tool{}))
 	return c
 }
