b061f67fba365c5deba7535c72856666b09ac568
Matt Hamilton <matt@soluble.ai>
2020-11-06 05:39:09 -0500

    dump printed hash
    Signed-off-by: Matt Hamilton <matt@soluble.ai>

--- pkg/tools/cfn-python-lint/cfn-python-lint.go
+++ pkg/tools/cfn-python-lint/cfn-python-lint.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/soluble-ai/go-jnode"
 	"github.com/soluble-ai/soluble-cli/pkg/log"
 	"github.com/soluble-ai/soluble-cli/pkg/tools"
@@ -32,10 +33,11 @@ func (t *Tool) Run() (*tools.Result, error) {
 	// #nosec G204
 	c := exec.Command("docker", "run", "--volume", fmt.Sprintf("%s:%s:ro", t.Directory, "/data"), "--detach",
 		"gcr.io/soluble-repo/soluble-cfn-lint:latest",
-		"/data/*.yaml", "/data/*.yml", "/data/*.json") // not sure if it will handle .template files correctly...
+		"/data/*.yaml", "/data/*.yml", "/data/*.json", "/data/*.template")
 	log.Infof("Running {primary:%s}", strings.Join(c.Args, " "))
 	c.Stderr = os.Stderr
 	d, err := c.Output()
+	spew.Dump(string(d))
 	if err != nil {
 		if d != nil {
 			os.Stderr.Write(d)
