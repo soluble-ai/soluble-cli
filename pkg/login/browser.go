1e9bd28230efee54162ed94746de30e0c6419cf9
Sam Shen <slshen@users.noreply.github.com>
2020-12-16 14:50:13 -0800

    Add headless login (#173)
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- pkg/login/browser.go
+++ pkg/login/browser.go
@@ -1,85 +1,51 @@
 package login
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/pkg/browser"
 	"github.com/soluble-ai/go-jnode"
 	"github.com/soluble-ai/soluble-cli/pkg/log"
-	"github.com/soluble-ai/soluble-cli/pkg/util"
 )
 
-type BrowserFlow struct {
-	appURL          string
-	state           string
-	endpoint        *callbackEndpoint
-	endpointPort    int
-	openBrowserFunc func(string) error
-	http            *http.Client
+type BrowserLeg struct {
+	openBrowserFunc func(url string) error
 }
 
-func NewBrowserFlow(appURL string, state string) Flow {
-	return &BrowserFlow{
-		appURL:          appURL,
-		state:           state,
-		endpoint:        &callbackEndpoint{origin: appURL},
-		openBrowserFunc: browser.OpenURL,
-		http:            &http.Client{},
-	}
+func (f *BrowserLeg) startEndpoint(appURL string) (*callbackEndpoint, int, error) {
+	endpoint := &callbackEndpoint{origin: appURL}
+	endpointPort, err := endpoint.listen()
+	return endpoint, endpointPort, err
 }
 
-func (f *BrowserFlow) Run() (*Response, error) {
-	var err error
-	f.endpointPort, err = f.endpoint.listen()
+func (f *BrowserLeg) GetCode(appURL string, state string) (string, error) {
+	endpoint, endpointPort, err := f.startEndpoint(appURL)
+	defer func() { _ = endpoint.Close() }()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	log.Infof("Started local webserver on port %d", f.endpointPort)
-	authURL := fmt.Sprintf("%s/api/v1/auth/cli-login/%s/%d", f.appURL, f.state, f.endpointPort)
+	log.Infof("Started local webserver on port %d", endpointPort)
+	authURL := fmt.Sprintf("%s/api/v1/auth/cli-login/%s/%d", appURL, state, endpointPort)
 	log.Infof("Opening browser URL {primary:%s}", authURL)
-	ch := f.endpoint.serveOne()
-	if err := f.openBrowserFunc(authURL); err != nil {
-		return nil, err
+	ch := endpoint.serveOne()
+	openBrowserFunc := f.openBrowserFunc
+	if openBrowserFunc == nil {
+		openBrowserFunc = browser.OpenURL
+	}
+	if err := openBrowserFunc(authURL); err != nil {
+		log.Warnf("No browser? Try running again in headless mode: {warning:soluble login --headless}")
+		return "", err
 	}
 	var n *jnode.Node
 	select {
 	case n = <-ch:
 	case <-time.After(time.Minute):
-		return nil, fmt.Errorf("authentication timed out")
+		return "", fmt.Errorf("authentication timed out")
 	}
 	if !n.IsObject() || n.Path("code").AsText() == "" {
-		log.Infof("{info:%s} did not return a successful authentication code", f.appURL)
-		return nil, fmt.Errorf("no response from servr")
+		log.Infof("{info:%s} did not return a successful authentication code", appURL)
+		return "", fmt.Errorf("no response from servr")
 	}
-	tokenURL := fmt.Sprintf("%s/api/v1/auth/cli-login-code", f.appURL)
-	log.Infof("Getting authentication token from {primary:%s}", tokenURL)
-	resp, err := f.http.PostForm(tokenURL, url.Values{
-		"state": []string{f.state},
-		"code":  []string{n.Path("code").AsText()},
-	})
-	if err != nil {
-		return nil, fmt.Errorf("couldn't connect to server: %w", err)
-	}
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("server returned %d", resp.StatusCode)
-	}
-	dat, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("garbled response: %w", err)
-	}
-	result := Response{}
-	if err := json.Unmarshal(dat, &result); err != nil {
-		return nil, fmt.Errorf("invalid response: %w", err)
-	}
-	result.defaultAPIServer(f.appURL)
-	return &result, nil
-}
-
-func (f *BrowserFlow) Close() error {
-	return util.CloseAll(f.endpoint)
+	return n.Path("code").AsText(), nil
 }
--- pkg/login/flow.go
+++ pkg/login/flow.go
@@ -1,6 +1,15 @@
 package login
 
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/soluble-ai/soluble-cli/pkg/log"
+)
 
 type Response struct {
 	UserID    string `json:"userId"`
@@ -9,9 +18,58 @@ type Response struct {
 	APIServer string `json:"apiServer"`
 }
 
-type Flow interface {
-	Run() (*Response, error)
-	Close() error
+type Flow struct {
+	authCodeLeg AuthCodeLeg
+	appURL      string
+	state       string
+	http        *http.Client
+}
+
+type AuthCodeLeg interface {
+	GetCode(appURL, state string) (string, error)
+}
+
+func NewFlow(appURL string, headless bool) *Flow {
+	flow := &Flow{
+		state:  MakeState(),
+		appURL: appURL,
+		http:   &http.Client{},
+	}
+	if headless {
+		flow.authCodeLeg = &HeadlessLeg{}
+	} else {
+		flow.authCodeLeg = &BrowserLeg{}
+	}
+	return flow
+}
+
+func (f *Flow) Run() (*Response, error) {
+	code, err := f.authCodeLeg.GetCode(f.appURL, f.state)
+	if err != nil {
+		return nil, err
+	}
+	tokenURL := fmt.Sprintf("%s/api/v1/auth/cli-login-code", f.appURL)
+	log.Infof("Getting authentication token from {primary:%s}", tokenURL)
+	resp, err := f.http.PostForm(tokenURL, url.Values{
+		"state": []string{f.state},
+		"code":  []string{code},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("couldn't connect to server: %w", err)
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("server returned %d", resp.StatusCode)
+	}
+	dat, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("garbled response: %w", err)
+	}
+	result := Response{}
+	if err := json.Unmarshal(dat, &result); err != nil {
+		return nil, fmt.Errorf("invalid response: %w", err)
+	}
+	result.defaultAPIServer(f.appURL)
+	return &result, nil
 }
 
 func (r *Response) defaultAPIServer(appURL string) {
