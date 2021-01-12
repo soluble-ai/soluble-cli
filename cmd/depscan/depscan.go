f6464d0382a69e555cdcf03f7cb9e6c77136226c
Matt Hamilton <matt@soluble.ai>
2021-01-12 05:49:39 -0500

    use flags, not subcommands
    Signed-off-by: Matt Hamilton <matt@soluble.ai>

--- cmd/depscan/depscan.go
+++ cmd/depscan/depscan.go
@@ -1,6 +1,7 @@
 package depscan
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/soluble-ai/soluble-cli/pkg/options"
@@ -18,20 +19,40 @@ func (opts *DepScanOpts) Register(c *cobra.Command) {
 }
 
 func Command() *cobra.Command {
-	return depScanCommand()
-}
-
-func depScanCommand() *cobra.Command {
+	var scanTools []string
 	opts := &DepScanOpts{}
 	c := &cobra.Command{
 		Use:   "dep-scan",
 		Short: "Scan dependencies in the local directory",
 		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(scanTools) == 0 {
+				scanTools = append(scanTools, "trivy")
+			}
+			pwd, _ := os.Getwd()
+			var errs []error
+			for _, tool := range scanTools {
+				switch tool {
+				case "trivy":
+					c := tools.CreateCommand(&trivy.DirTool{
+						Dir: pwd,
+					})
+					errs = append(errs, c.RunE(c, nil))
+				default:
+					return fmt.Errorf("invalid tool: %q", tool)
+				}
+			}
+			for _, err := range errs {
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
 	}
-	pwd, _ := os.Getwd()
-	c.AddCommand(tools.CreateCommand(&trivy.DirTool{
-		Dir: pwd,
-	}))
+
 	opts.Register(c)
+	flags := c.Flags()
+	flags.StringSliceVarP(&scanTools, "tools", "t", nil, "Scanning tool. Defaults to \"trivy\"")
 	return c
 }
