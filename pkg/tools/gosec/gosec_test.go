08f4c1003266911dac6ac85105d5a4954dd62372
Hemanth Gokavarapu <hemanth@soluble.ai>
2021-04-07 00:29:49 +0530

    Adding support for gosec in Soluble CLI (#246)
    * Adding support for gosec in Soluble CLI
    
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- pkg/tools/gosec/gosec_test.go
+++ pkg/tools/gosec/gosec_test.go
@@ -12,25 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package codescan
+package gosec
 
 import (
-	"github.com/soluble-ai/soluble-cli/pkg/tools"
-	"github.com/soluble-ai/soluble-cli/pkg/tools/bandit"
-	"github.com/soluble-ai/soluble-cli/pkg/tools/gosec"
-	"github.com/soluble-ai/soluble-cli/pkg/tools/semgrep"
-	"github.com/spf13/cobra"
+	"testing"
+
+	"github.com/soluble-ai/soluble-cli/pkg/util"
+	"github.com/stretchr/testify/assert"
 )
 
-func Command() *cobra.Command {
-	c := &cobra.Command{
-		Use:   "code-scan",
-		Short: "Scan code with a variety of static analysis tools",
-	}
-	c.AddCommand(
-		tools.CreateCommand(&semgrep.Tool{}),
-		tools.CreateCommand(&bandit.Tool{}),
-		tools.CreateCommand(&gosec.Tool{}),
-	)
-	return c
+func TestParseResults(t *testing.T) {
+	assert := assert.New(t)
+	results, err := util.ReadJSONFile("testdata/results.json.gz")
+	assert.Nil(err)
+	tool := &Tool{}
+	result := tool.parseResults(results)
+	assert.Equal(1, len(result.Findings))
+	assert.Equal(1, result.Data.Path("Issues").Size())
+	f := result.Findings[0].Tool
+	assert.Equal("G101", f["rule_id"])
+	assert.Equal(results.Unwrap(), result.Data.Unwrap())
 }
