f1cbeac274021679d49b54d59798cf38aff724c1
Sam Shen <slshen@users.noreply.github.com>
2021-10-19 17:33:26 -0700

    Report errors from checkov helm scan when checkov fails. (#302)
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- pkg/tools/checkov/helm.go
+++ pkg/tools/checkov/helm.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/soluble-ai/soluble-cli/pkg/tools"
 )
 
@@ -18,6 +19,7 @@ func (h *Helm) Name() string {
 }
 
 func (h *Helm) Run() (*tools.Result, error) {
+	var errs error
 	inventory := h.GetInventory()
 	for _, chart := range inventory.HelmCharts.Values() {
 		checkov := &Tool{
@@ -31,16 +33,20 @@ func (h *Helm) Run() (*tools.Result, error) {
 		}
 		result, err := checkov.RunTool(false)
 		if err != nil {
-			return nil, err
+			// checkov crashes if the chart is malformed, so just
+			// accumulate the errors but keep going with other charts
+			errs = multierror.Append(errs,
+				fmt.Errorf("checkov failed on %s - %w", checkov.Directory, err))
+		} else {
+			// Print the result now, but change file_path so that it's
+			// relative to the chart directory.  Note that this only
+			// affects printing of the result in the CLI, the result
+			// has already been uploaded with the correct repo path.
+			for _, f := range result.Findings {
+				f.FilePath = fmt.Sprintf("%s/%s", chart, f.FilePath)
+			}
+			checkov.PrintToolResult(result)
 		}
-		// Print the result now, but change file_path so that it's
-		// relative to the chart directory.  Note that this only
-		// affects printing of the result in the CLI, the result
-		// has already been uploaded with the correct repo path.
-		for _, f := range result.Findings {
-			f.FilePath = fmt.Sprintf("%s/%s", chart, f.FilePath)
-		}
-		checkov.PrintToolResult(result)
 	}
-	return nil, nil
+	return nil, errs
 }
