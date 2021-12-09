75dba0f8fe4493300d915b9bd51158b43408581c
Sam Shen <slshen@users.noreply.github.com>
2021-12-09 10:05:15 -0800

    Remove --update-pr option from tools
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- pkg/assessments/pr.go
+++ pkg/assessments/pr.go
@@ -33,12 +33,13 @@ type PRIntegrations []func(context.Context, *jnode.Node) PRIntegration
 
 var prIntegrations = PRIntegrations{}
 
-func RegisterPRIntegration(integ func(context.Context, *jnode.Node) PRIntegration) {
+func RegisterPRIntegration(integ func(ctx context.Context, config *jnode.Node) PRIntegration) {
 	prIntegrations = append(prIntegrations, integ)
 }
 
-func getCIIntegrationToken(client *api.Client) *jnode.Node {
-	res, err := client.Post("/api/v1/org/{org}/git/ci-token", nil, xcp.WithCIEnvBody(""))
+func getCIIntegrationConfig(client *api.Client) *jnode.Node {
+	// NB - not implemented in api-server
+	res, err := client.Post("/api/v1/org/{org}/ci-integration-config", nil, xcp.WithCIEnvBody(""))
 	if err != nil {
 		if !errors.Is(err, api.HTTPError) {
 			log.Warnf("Could not get CI integration config: {danger:%s}", err)
@@ -48,13 +49,14 @@ func getCIIntegrationToken(client *api.Client) *jnode.Node {
 	return res
 }
 
+// NB - not used
 func (a *Assessment) UpdatePR(client *api.Client) error {
 	as := Assessments{a}
 	return as.UpdatePR(client)
 }
 
 func (a Assessments) UpdatePR(client *api.Client) error {
-	token := getCIIntegrationToken(client)
+	token := getCIIntegrationConfig(client)
 	if token == nil {
 		return fmt.Errorf("pull request integration has not been setup for this repository")
 	}
