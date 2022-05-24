994f07c6c6a0c17a8a07ce911b8512c143cfc603
Sam Shen <slshen@users.noreply.github.com>
2022-05-24 07:54:26 -0700

    Add KustomizeDirectories to inventory (#417)
    * And remove kubernetes manifests found in kustomize directories
    
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- pkg/inventory/inventory_test.go
+++ pkg/inventory/inventory_test.go
@@ -13,5 +13,8 @@ func TestInventory(t *testing.T) {
 		assert.ElementsMatch(m.PythonDirectories.Values(), []string{"lang/python-app"})
 		assert.ElementsMatch(m.JavaDirectories.Values(), []string{"lang/java", "lang/java2", "lang/java3"})
 		assert.ElementsMatch(m.TerraformRootModules.Values(), []string{"tf/r1", "tf/r1j", "tf/r2"})
+		assert.ElementsMatch(m.KustomizeDirectories.Values(), []string{"k/kus"})
+		assert.ElementsMatch(m.KubernetesManifestDirectories.Values(), []string{"k/t"})
+		assert.ElementsMatch(m.HelmCharts.Values(), []string{"k/h"})
 	}
 }
