a82aed7f93986c49d1c494a121ddfd691b1f99c3
Hemanth Gokavarapu <5942165+hemanthgk10@users.noreply.github.com>
2022-09-15 17:18:24 +0530

    Fix the Jenkins attribute mapping and other important standard variables for CLI and exclude few more sensitive information (#501)
    * Fix the Jenkins attribute mapping and other important standard variables for CLI and exclude few more sensitive information
    
    Signed-off-by: Hemanth Gokavarapu <hemanth@soluble.ai>

--- pkg/xcp/xcp_test.go
+++ pkg/xcp/xcp_test.go
@@ -44,6 +44,49 @@ func TestGetCIEnv(t *testing.T) {
 	os.Setenv("ATLANTIS_TERRAFORM_VERSION", "yyy")
 	os.Setenv("PULL_NUM", "yyy")
 	os.Setenv("REPO_REL_DIR", "yyy")
+	os.Setenv("BUILD_ID", "27")
+	os.Setenv("JOB_BASE_NAME", "main")
+	os.Setenv("KUBERNETES_PORT", "tcp://172.20.0.1:443")
+	os.Setenv("RUN_ARTIFACTS_DISPLAY_URL", "https://ci.intouchhealth.io/")
+	os.Setenv("TF_VAR_adminpassword", "****")
+	os.Setenv("TF_VAR_adminusername", "***")
+	os.Setenv("TF_VAR_adminusername", "***")
+	os.Setenv("ARM_TENANT_ID", "****")
+	os.Setenv("ARM_CLIENT_SECRET", "yyy")
+	env := GetCIEnv(".")
+	for k, v := range env {
+		if v == "xxx" {
+			t.Error(k, v)
+		}
+	}
+
+	assert.True(contains(env, "BUILD_ID"))
+	assert.True(contains(env, "JOB_BASE_NAME"))
+	assert.True(contains(env, "KUBERNETES_PORT"))
+	assert.True(contains(env, "SOLUBLE_METADATA_CI_SYSTEM"))
+	assert.True(contains(env, "RUN_ARTIFACTS_DISPLAY_URL"))
+	assert.False(contains(env, "TF_VAR_adminpassword"))
+	assert.False(contains(env, "TF_VAR_adminusername"))
+	assert.False(contains(env, "ARM_TENANT_ID"))
+	assert.False(contains(env, "ARM_CLIENT_SECRET"))
+}
+
+func TestAtlantisCIEnv(t *testing.T) {
+	assert := assert.New(t)
+	saveEnv := os.Environ()
+	defer func() {
+		os.Clearenv()
+		for _, kv := range saveEnv {
+			eq := strings.Index(kv, "=")
+			os.Setenv(kv[0:eq], kv[eq+1:])
+		}
+	}()
+
+	os.Setenv("ATLANTIS_TERRAFORM_VERSION", "yyy")
+	os.Setenv("PULL_NUM", "yyy")
+	os.Setenv("REPO_REL_DIR", "yyy")
+	os.Setenv("BUILD_ID", "27")
+	os.Setenv("JOB_BASE_NAME", "main")
 	env := GetCIEnv(".")
 	for k, v := range env {
 		if v == "xxx" {
@@ -54,7 +97,6 @@ func TestGetCIEnv(t *testing.T) {
 	// make sure atlantis env variables are available
 	assert.True(contains(env, "ATLANTIS_TERRAFORM_VERSION"))
 	assert.True(contains(env, "ATLANTIS_PULL_NUM"))
-
 	for _, kv := range os.Environ() {
 		if strings.HasSuffix(kv, "=yyy") {
 			if strings.HasPrefix(kv, "PULL_NUM") ||
