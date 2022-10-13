ef8a747b652653404be4359e1cc463c5210c3915
Sam Shen <slshen@users.noreply.github.com>
2022-10-13 12:13:37 -0700

    Don't try and use custom rules for secrets, tfsec, or semgrep. (#514)
    These tools do not yet have (our) support for custom rules.
    
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>
    
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- pkg/tools/opal/opal.go
+++ pkg/tools/opal/opal.go
@@ -96,7 +96,7 @@ func (t *Tool) Run() (*tools.Result, error) {
 	if err != nil {
 		return nil, err
 	}
-	customPoliciesDir, err := t.GetCustomPoliciesDir()
+	customPoliciesDir, err := t.GetCustomPoliciesDir("opal")
 	if err != nil {
 		return nil, err
 	}
