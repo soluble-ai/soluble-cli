ac2f54030ebaaf6b13b0ee58562e6d7c52b5a8eb
Sam Shen <slshen@users.noreply.github.com>
2022-10-21 13:20:20 -0700

    fix(RAIN-38965) policy download does not work using lacework auth (#521)
    * download sets the correct headers when using lacework auth
    
    * fix: getting an unauth client wipes out auth headers
    
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>
    
    Signed-off-by: Sam Shen <slshen@users.noreply.github.com>

--- pkg/download/options.go
+++ pkg/download/options.go
@@ -17,10 +17,3 @@ package download
 import "net/http"
 
 type downloadOption func(req *http.Request) error
-
-func withBearerToken(token string) downloadOption {
-	return func(req *http.Request) error {
-		req.Header.Set("Authorization", "Bearer "+token)
-		return nil
-	}
-}
