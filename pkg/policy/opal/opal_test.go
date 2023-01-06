645b4798f7ed713fa7b007638df87463daac19e4
Ellie <ellie.goggin@lacework.net>
2023-01-06 17:30:16 +0000

    Add tests
    Signed-off-by: Ellie <ellie.goggin@lacework.net>

--- pkg/policy/opal/opal_test.go
+++ pkg/policy/opal/opal_test.go
@@ -1,7 +1,14 @@
 package opal
 
 import (
+	"net/http"
 	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/soluble-ai/soluble-cli/pkg/api"
+	"github.com/soluble-ai/soluble-cli/pkg/options"
+	"github.com/soluble-ai/soluble-cli/pkg/tools"
+	"github.com/soluble-ai/soluble-cli/pkg/tools/opal"
 
 	"github.com/soluble-ai/soluble-cli/pkg/policy/manager"
 	"github.com/stretchr/testify/assert"
@@ -30,4 +37,43 @@ func TestPoliciesFail(t *testing.T) {
 	assert.Error(err)
 	assert.Equal(1, tm.Failed)
 	assert.Equal(1, tm.Passed)
+}
+
+func TestGetCustomPoliciesDir(t *testing.T) {
+	assert := assert.New(t)
+	apiConfig := &api.Config{
+		APIServer:        "https://api.test",
+		APIPrefix:        "/api/v1",
+		Organization:     "1234",
+		LegacyAPIToken:   "token",
+		LaceworkAPIToken: "token",
+	}
+	c := api.NewClient(apiConfig)
+	o := &tools.AssessmentOpts{
+		ToolOpts: tools.ToolOpts{
+			RunOpts: tools.RunOpts{
+				PrintClientOpts: options.PrintClientOpts{
+					ClientOpts: options.ClientOpts{
+						Client: &api.Client{
+							Client: c.Client,
+							Config: *apiConfig,
+						},
+					},
+				},
+			},
+			Tool: &opal.Tool{},
+		},
+	}
+	httpmock.ActivateNonDefault(o.ToolOpts.RunOpts.ClientOpts.Client.Client.GetClient())
+	defer httpmock.DeactivateAndReset()
+	httpmock.RegisterResponder(http.MethodGet,
+		"https://api.test/api/v1/org/1234/policies/opal/policies.zip",
+		httpmock.NewBytesResponder(http.StatusNoContent, []byte{}))
+	httpmock.RegisterNoResponder(httpmock.NewBytesResponder(http.StatusNoContent, []byte{}))
+
+	customPoliciesDir, err := o.GetCustomPoliciesDir("opal")
+
+	assert.Equal(1, httpmock.GetTotalCallCount())
+	assert.NoError(err)
+	assert.Equal(customPoliciesDir, "")
 }
