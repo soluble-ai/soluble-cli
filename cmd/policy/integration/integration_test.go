77b512cd7e276538dc8bf6c611f44db31c4ce9f0
James Stocks <james.stocks@lacework.net>
2023-04-14 10:52:08 +0100

    fix(COD-652): Integration test for policy create wizard

--- cmd/policy/integration/integration_test.go
+++ cmd/policy/integration/integration_test.go
@@ -3,8 +3,14 @@
 package integration
 
 import (
+	"github.com/ActiveState/vt10x"
+	"github.com/Netflix/go-expect"
+	"os"
+	"os/exec"
+	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/soluble-ai/soluble-cli/cmd/test"
 	"github.com/stretchr/testify/assert"
@@ -46,4 +52,69 @@ func TestPolicyUploadFail(t *testing.T) {
 		"no policies found"+
 			"\n\t - Ensure path provided points to the parent directory of the /policies directory"+
 			"\n\t - or use --allow-empty to upload no policies")
+}
+
+func TestPolicyCreateWizard(t *testing.T) {
+
+	assert := assert.New(t)
+
+	// Create a virtual terminal
+	c, _, err := vt10x.NewVT10XConsole()
+	assert.NoError(err)
+	defer c.Close()
+
+	// Define command for `policy create` and attach it to the virtual terminal
+	cmd := exec.Command("go", "run", "../../../main.go", "policy", "create")
+	cmd.Stdin = c.Tty()
+	cmd.Stdout = c.Tty()
+	cmd.Stderr = c.Tty()
+
+	description := "This is my sample custom policy. There are many like it, but this one is mine."
+	// Run through the wizard
+	cmd.Start()
+	expectAndRespond(assert, c, `.*Policies directory path.*`, "test_custom_policies", 5)
+	expectAndRespond(assert, c, `.*Create policies directory here.*`, "y", 1)
+	defer os.RemoveAll("test_custom_policies")
+	expectAndRespond(assert, c, `Select provider`, "aws", 1)
+	expectAndRespond(assert, c, `Select target`, "terraform", 1)
+	expectAndRespond(assert, c, `policy name`, "sample_custom_policy", 1)
+	expectAndRespond(assert, c, `Title`, "My Sample Custom Policy", 1)
+	expectAndRespond(assert, c, `Description`, description, 1)
+	expectAndRespond(assert, c, `Category`, "storage", 1)
+	expectAndRespond(assert, c, `ResourceType`, "aws_s3_bucket", 1)
+	expectAndRespond(assert, c, `Select severity`, "info", 1)
+	c.Tty().Close()
+	out, err := c.ExpectEOF()
+
+	// Test the result of the wizard
+	createdPolicyDir := "test_custom_policies/policies/opal/sample_custom_policy"
+	assert.Contains(out, "created:  "+createdPolicyDir)
+	_, err = os.Stat(createdPolicyDir)
+	assert.NoError(err)
+	_, err = os.Stat(createdPolicyDir + "/metadata.yaml")
+	assert.NoError(err)
+	metadata, err := os.ReadFile(createdPolicyDir + "/metadata.yaml")
+	expected := regexp.MustCompile("description: \"" + description + "\"")
+	matches := expected.FindStringSubmatch(string(metadata))
+	if matches == nil {
+		assert.Fail("Created policy doesn't have expected description in metadata.yaml")
+	}
+	_, err = os.Stat(createdPolicyDir + "/terraform/policy.rego")
+	assert.NoError(err)
+	expected = regexp.MustCompile(`resource_type := "aws_s3_bucket"`)
+	policy, err := os.ReadFile(createdPolicyDir + "/terraform/policy.rego")
+	matches = expected.FindStringSubmatch(string(policy))
+	if matches == nil {
+		assert.Fail("Created policy doesn't have expected aws_s3_bucket in terraform/policy.rego")
+	}
+}
+
+func expectAndRespond(assert *assert.Assertions, c *expect.Console, exp string, response string, timeout int) {
+	_, err := c.Expect(expect.Regexp(regexp.MustCompile(exp)), expect.WithTimeout(time.Second*time.Duration(timeout)))
+	assert.NoError(err)
+
+	if response != "" {
+		_, err = c.SendLine(response)
+		assert.NoError(err)
+	}
 }
